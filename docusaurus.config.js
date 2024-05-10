049ee6c11f0d6384fe39620ce15e288f75d4a642
Nikita Tsvetkov <tsv1@fastmail.com>
2024-05-10 17:36:38 +0400

    update url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ import { themes as prismThemes } from 'prism-react-renderer';
 const config = {
   title: 'JJ',
   tagline: 'Remote HTTP Mock',
-  url: 'https://jj.vedro.io',
+  url: 'https://jj-mock.io',
   baseUrl: '/',
   trailingSlash: false,
   onBrokenLinks: 'throw',
